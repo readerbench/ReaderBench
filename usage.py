46473280edda98d08080b5c8c6ceec9f73d70575
Teodor-Mihai Cotet <teodor.cotet@gmail.com>
2019-06-12 18:53:47 +0300

    readme update

--- usage.py
+++ usage.py
@@ -8,11 +8,9 @@ from rb.similarity.wordnet import path_similarity, get_hypernyms, get_all_paths_
 from rb.complexity.word.name_entity_enum import NamedEntityONEnum
 from nltk.corpus import wordnet as wn
 
-
 txt1 = """This is a sample document. It Romanian, (him) can contain, multiple sentences and paragraphs and repeating sentencesm Romania.
 This is considered a new block (paragraph).
 Therefore in total are 3 blocks."""
-
 
 txt2 = """S-a născut repede la 1 februarie 1852,[3] în satul Haimanale (care astăzi îi poartă numele), fiind primul născut al lui Luca Ștefan Caragiale și al Ecaterinei Chiriac Karaboas. Conform unor surse, familia sa ar fi fost de origine aromână.[6] Tatăl său, Luca (1812 - 1870), și frații acestuia, Costache și Iorgu, s-au născut la Constantinopol, 
 fiind fiii lui Ștefan, un bucătar angajat la sfârșitul anului 1812 de Ioan Vodă Caragea în suita sa."""
@@ -22,7 +20,9 @@ docs_en = Document(Lang.EN, txt1)
 parser = SpacyParser.get_instance().get_model(Lang.EN)
 parsed = parser(txt1)
 
+
 """how to use wordnet for RO"""
+
 print(POS.NOUN.to_wordnet())
 print('hypernyms ro', get_hypernyms('om', lang=Lang.RO, pos=POS.NOUN.to_wordnet()))
 print('hypernyms eng', get_hypernyms('human', lang=lang_dict[Lang.EN]))
@@ -31,6 +31,7 @@ print('pe', get_all_paths_lengths_to_root('pe', lang=Lang.RO))
 """how to parse text"""
 for sent in parsed.sents:
         print(sent, sent.root)
+
 
 """how to compute indices"""
 compute_indices(docs_en)
@@ -53,18 +54,16 @@ for comp in docs_en.components:
         for key, v in comp2.indices.items():
             print(comp2.text, key, v)
 
-"""how to use named entity for ro"""
 
+"""how to use named entity for ro, how to extract content words"""
+print('parsed text, lemmas, content words, ent types, etc.')
 docs_ro = Document(Lang.RO, txt2)
-
 for comp1 in docs_ro.components:
 	# comp is para
     for comp2 in comp1.components:
 		# comp2 is sent
         for ent in comp2.entities:
             print(ent.text, 'x')
-
         # key is word
         for key in comp2.components:
-            if key.is_content_word():
-                print(key.lemma, key.is_stop, key.pos, key.ent_type, key.ent_type_, key.tag, key.is_content_word())
+            print(key.lemma, key.is_stop, key.pos, key.ent_type, key.ent_type_, key.tag, key.is_content_word())
