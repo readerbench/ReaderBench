7d153759352039f28b6fda588ba6503552f4c92a
Stefan Ruseti <stefan.ruseti@cs.pub.ro>
2019-06-20 16:20:15 +0300

    initial span get_root

--- rb/core/span.py
+++ rb/core/span.py
@@ -10,3 +10,6 @@ class Span(TextElement):
     def __init__(self, lang: Lang, text: str, words: List[Word], depth: int = TextElementType.SPAN.value, container: TextElement=None):
         super().__init__(lang, text, depth, container=container)
         self.words = words
+
+    def get_root(self) -> Word:
+        return [word for word in self.words if word.head == word or word.head.i < self.words[0].i or word.head.i > self.words[-1].i][0]
