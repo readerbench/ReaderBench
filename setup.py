d68810baaaa7e1700d907c05f40dda325b1837a8
Dragos Georgian <dragos@readerbench.com>
2019-06-25 10:13:13 +0000

    Modified the pypi deployment to postinstall additional requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,55 @@
 import setuptools
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+def do_post_install_tasks():
+    # download spacy thing
+    check_call("pip3 install https://github.com/explosion/spacy-models/releases/download/xx_ent_wiki_sm-2.1.0/xx_ent_wiki_sm-2.1.0.tar.gz --user".split())
+
+    # download nltk stuff
+    from os import getenv, path
+
+    import nltk
+
+    from rb.core.lang import Lang
+    from rb.utils.downloader import download_wordnet
+
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    nltk.download('omw')
+
+    nltk_path = getenv('NLTK_DATA')
+    if nltk_path is None:
+        nltk_path = path.expanduser('~/nltk_data/')
+    download_wordnet(Lang.RO, nltk_path + "corpora/omw")
+
+
+class PostDevelopmentCommand(develop):
+    """Post-instalation for development? mode."""
+    def run(self):
+        do_post_install_tasks()
+        # run
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-instalation for install mode."""
+    def run(self):
+        do_post_install_tasks()
+
+        # run
+        install.run(self)
+
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.3.2',
+    version='0.4.8',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
     description='ReaderBench library written in python',
@@ -26,6 +70,10 @@ setuptools.setup(
           'neuralcoref',
           'joblib'
       ],
+    cmdclass={
+        'develop': PostDevelopmentCommand,
+        'install': PostInstallCommand,
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
