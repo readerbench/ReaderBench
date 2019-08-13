8f8276dbe8a6e29d165216150dd47f8855a04ea0
Stefan Ruseti <stefan.ruseti@cs.pub.ro>
2019-08-13 17:59:14 +0300

    added missing default methods for Vector class

--- rb/similarity/vector.py
+++ rb/similarity/vector.py
@@ -11,3 +11,17 @@ class Vector:
     @staticmethod
     def cosine_similarity(a: "Vector", b: "Vector") -> float:
         return np.dot(a.values, b.values) / (a.norm * b.norm)
+
+    def __len__(self):
+        return self.size
+
+    def __eq__(self, value):
+        if not isinstance(value, Vector):
+            return False
+        return self.values == value.values
+
+    def __hash__(self):
+        return self.values.__hash__()
+
+    def __repr__(self):
+        return self.values.__repr__()
