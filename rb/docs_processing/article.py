5dd2bd43ef94c116ca5e07a2aa4201016b19df3f
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2020-01-23 09:55:20 +0200

    Graph refactoring + centrality + clustering (wip)

--- rb/docs_processing/article.py
+++ rb/docs_processing/article.py
@@ -10,32 +10,51 @@ import datetime
 
 class Article:
 
-    def __init__(self, title: str, abstract: str, source:str, authors: List[str], date: str, lang: Lang, graph: "Graph"):
+    def __init__(self, title: str, abstract: str, source: str, authors: List[str], date: str, lang: Lang):
         self.title = title
         self.abstract = abstract
+
         self.author_list = []
-        
-        for author_name in authors:
-            if author_name:
-                self.author_list.append(author_name)
-                if not author_name in graph.authors_dict:
-                    author = Author(author_name)
-                    graph.authors_dict[author_name] = author
-                    author.articles.add(self)
-                else:
-                    graph.authors_dict[author_name].articles.add(self)
+        self.extract_authors(authors)
         
         self.source = source
         self.date = datetime.datetime.strptime(date, "%d-%m-%Y")
         self.document = Document(lang, abstract)
+
+    def extract_authors(self, authors):
+        for author_name in authors:
+            if not author_name:
+                continue
+            self.author_list.append(author_name.strip())
+
+    def add_this_article_to_its_authors(self, graph):
+        for author_name in self.author_list:
+            self.add_this_article_to_author(author_name, graph)
+
+    def add_this_article_to_author(self, author_name, graph):
+        if author_name not in graph.authors_dict:
+            author = Author(author_name)
+            author.articles.add(self)
+            graph.authors_dict[author_name] = author
+        else:
+            graph.authors_dict[author_name].articles.add(self)
+
+    def add_this_article_to_the_graph(self, graph):
         graph.articles_set.add(self)
 
-    
     def __eq__(self, other):
         if isinstance(other, Article):
             return self.title == other.title
         return NotImplemented
 
-
     def __hash__(self):
         return hash(tuple(self.title))
+
+
+def create_article_and_add_it_to_its_authors_and_graph(title: str, abstract: str, source: str,
+                                                       authors: List[str], date: str, lang: Lang, graph):
+    article = Article(title, abstract, source, authors, date, lang)
+    article.add_this_article_to_its_authors(graph)
+    article.add_this_article_to_the_graph(graph)
+
+    return article
