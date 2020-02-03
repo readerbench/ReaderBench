3e72c6400055fd0452f519ff4ea72f7a26f0d354
Gabi Gutu <gabi.gutu@gmail.com>
2020-02-03 15:41:03 +0200

    Added Jose_Antonio for keyword extraction

--- rb/processings/keywords/keywords_extractor.py
+++ rb/processings/keywords/keywords_extractor.py
@@ -25,6 +25,9 @@ class KeywordExtractor():
         elif lang is Lang.EN:
             vector_model = create_vector_model(
                 Lang.EN, VectorModelType.from_str("word2vec"), "coca")
+        elif lang is Lang.ES:
+            vector_model = create_vector_model(
+                Lang.ES, VectorModelType.from_str("word2vec"), "jose_antonio") 
         else:
             logger.error(
                 f'Language {lang.value} is not supported for keywords task')
