e0b8dc3531c3afbf272b81678a547c41defeae18
teo <teodor.cotet@gmail.com>
2020-05-14 18:25:29 +0300

    gec

--- main_gec.py
+++ main_gec.py
@@ -3,5 +3,5 @@ from rb.processings.gec.gec_correct import GecCorrector
 if __name__ == "__main__":
     
     corrector = GecCorrector(d_model=768)
-    decoded_senntence = corrector.correct_sentence('Am mers la magazi sa cuumper leegume si fructe de mancaat.')
+    decoded_senntence = corrector.correct_sentence('Am meers l-a magaazin sa cuumper s leegume si fructe de mancaat.')
     print(decoded_senntence)
