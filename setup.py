ad5a1fd919c180804eff87df59615d68061d8ae0
Gabi Gutu <gabi.gutu@gmail.com>
2020-06-10 03:41:29 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.8.2',
+    version='0.8.3',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
     description='ReaderBench library written in python',
