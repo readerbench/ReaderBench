4603c2ed4538ce3853023ce9340c26ae55801958
Stefan <stefan.ruseti@gmail.com>
2020-06-10 11:26:18 +0300

    version update

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.8.3',
+    version='0.8.4',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
     description='ReaderBench library written in python',
