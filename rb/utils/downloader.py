b475ccd269626b40d9da650dff9fd28c6af60538
Mihai-Dan MAŞALA (25095) <mihai_dan.masala@stud.acs.upb.ro>
2020-12-21 17:41:19 +0200

    New link for Ro.sentiment.base

--- rb/utils/downloader.py
+++ rb/utils/downloader.py
@@ -50,8 +50,8 @@ LINKS = {
                     "version": "https://nextcloud.readerbench.com/index.php/s/CF5TmPeEKFNib4H/download"
                 },
                 'base': {
-                    "link": "https://nextcloud.readerbench.com/index.php/s/kC98HTJbPmsXxBj/download",
-                    "version": "https://nextcloud.readerbench.com/index.php/s/ix5FRQHEbaP8CFw/download"
+                    "link": "https://nextcloud.readerbench.com/index.php/s/FwWy9TWNHymJ5Hy/download",
+                    "version": "https://nextcloud.readerbench.com/index.php/s/RCp9bgRPF6X3QDq/download"
                 },
                 'large': {
                     "link": "https://nextcloud.readerbench.com/index.php/s/pHXqLQ8HXar44WT/download",
