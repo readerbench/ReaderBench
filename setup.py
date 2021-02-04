017818ef6a468a08e3598da4e9197e3bc83e896e
Stefan Ruseti <stefan.ruseti@cs.pub.ro>
2021-02-04 20:37:14 +0200

    fixed BertWrapper bug loading Bert models with LM head

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.10.16',
+    version='0.10.17',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
     description='ReaderBench library written in python',
