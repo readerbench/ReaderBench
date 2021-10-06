6a365dd20a7bf3f274256872fa171558742d5b34
Dragos Corlatescu <Dragos Corlatescu>
2021-10-06 12:16:39 +0000

    Upgraded library version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.10.40',
+    version='0.10.41',
     python_requires='>=3.6,<3.9',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
