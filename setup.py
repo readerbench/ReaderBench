93db64f63b11cfc847381b74d9ba176d539edcc4
Stefan Ruseti <stefan.ruseti@cs.pub.ro>
2021-10-28 11:47:40 +0300

    moved requirements.txt to manifest

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 def do_post_install_tasks():
-    check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    check_call([sys.executable, "-m", "pip", "install", "-U", "-r", "requirements.txt"])
     # cwd = getcwd()
     # with TemporaryDirectory() as temp_folder:
     #     chdir(temp_folder)
@@ -57,7 +57,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.10.52',
+    version='0.10.53',
     python_requires='>=3.6,<3.10',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
@@ -68,7 +68,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=required,
-    data_files=[("",["requirements.txt"])],
     cmdclass={
         'develop': PostDevelopmentCommand,
         'install': PostInstallCommand,
