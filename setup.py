da7eb61fbc72c38ca1f55dc6d355f09f033ada2b
Stefan Ruseti <stefan.ruseti@cs.pub.ro>
2021-11-02 14:20:30 +0200

    * fixed an issue with transformer tokenization with weird characters * changed EN transformer model to sentence-transformers/all-distilroberta-v1 in order to improve text representation

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='rbpy-rb',
-    version='0.10.54',
+    version='0.10.55',
     python_requires='>=3.6,<3.10',
     author='Woodcarver',
     author_email='batpepastrama@gmail.com',
