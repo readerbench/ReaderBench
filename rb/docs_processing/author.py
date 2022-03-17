d5cec10b448b3ec937beacdf5eb21ea6653bb182
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2022-03-17 10:20:21 +0200

    Added str and repr for author

--- rb/docs_processing/author.py
+++ rb/docs_processing/author.py
@@ -10,6 +10,11 @@ class Author:
             return self.name == other.name
         return NotImplemented
 
-
     def __hash__(self):
         return hash(tuple(self.name))
+    
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self) -> str:
+        return self.name
