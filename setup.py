f100ba498d562b16e83167878d39dd203418c754
Stefan Ruseti <stefan.ruseti@gmail.com>
2022-07-25 15:25:49 +0300

    nltk wordnet resources changed to 'omw-1.4'

--- setup.py
+++ setup.py
@@ -41,22 +41,11 @@ def do_post_install_tasks():
         check_call([sys.executable, "-m", "pip", "install", "tensorflow", "pytorch", "transformers"])
 
     #  download nltk stuff
-    from os import getenv, path
-
     import nltk
-
-    from rb.core.lang import Lang
-    from rb.utils.downloader import download_wordnet
 
     nltk.download('punkt')
     nltk.download('wordnet')
-    nltk.download('omw')
-
-    nltk_path = getenv('NLTK_DATA')
-    if nltk_path is None:
-        nltk_path = path.expanduser('~/nltk_data/')
-    download_wordnet(Lang.RO, nltk_path + "corpora/omw")
-
+    nltk.download('omw-1.4')
 
 class PostDevelopmentCommand(develop):
     """Post-instalation for development? mode."""
